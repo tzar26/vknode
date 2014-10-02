418e7d7c2883eeca0d5f08e087cf92404b62adb5
Stepkin Alezander <stepkin@jetstyle.ru>
2014-10-02 17:44:37 +0600

    .

--- static/script.js
+++ static/script.js
@@ -115,6 +115,26 @@ var vk = {
 
 // alert(vk.data.user);
 // vk.getUserInfo(vk.data.user.id);
+var url_parser={
+        get_args: function (s) {
+            var tmp=new Array();
+            s=(s.toString()).split('&');
+            for (var i in s) {
+                i=s[i].split("=");
+                tmp[(i[0])]=i[1];
+            }
+            return tmp;
+        },
+        get_args_cookie: function (s) {
+            var tmp=new Array();
+            s=(s.toString()).split('; ');
+            for (var i in s) {
+                i=s[i].split("=");
+                tmp[(i[0])]=i[1];
+            }
+            return tmp;     
+        }
+};
 
 var plugin_vk = {
     wwwref: false,
