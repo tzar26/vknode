4b257ede1a67715a154ceab7561863a3f6ea5953
Stepkin Alezander <stepkin@jetstyle.ru>
2014-10-07 13:11:28 +0600

    добавлен конфиг, основной код вынесен в воркер

---
+++ configs/current/node.js
@@ -0,0 +1,14 @@
+module.exports = {
+    environment: 'development',
+    dirs: [
+    ],
+    node: {
+    },
+    app: {
+        version: '1',
+    },
+    db: {
+        host: 'localhost',
+        database: 'test',
+    },
+};
